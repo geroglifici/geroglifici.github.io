cf0e965a06602aa655f1dc4cc1abb78e158bb385
Stella Vaccari <stella@marcov.it>
2020-08-18 15:03:41 +0000

    Updates

---
+++ precache-manifest.8963e0cdac2e66e4c2c691322c431536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35656913ccd009b6b84d07e352bc3434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f21f548286b7f233b6f",
+    "url": "/static/css/2.c25c4777.chunk.css"
+  },
+  {
+    "revision": "4ae68e0b84cfd2f1f118",
+    "url": "/static/css/main.867faebd.chunk.css"
+  },
+  {
+    "revision": "8f21f548286b7f233b6f",
+    "url": "/static/js/2.2606488c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2606488c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae68e0b84cfd2f1f118",
+    "url": "/static/js/main.7dacc1db.chunk.js"
+  },
+  {
+    "revision": "a959c4261b80e1a5229a",
+    "url": "/static/js/runtime-main.9e9eba89.js"
+  }
+]);
